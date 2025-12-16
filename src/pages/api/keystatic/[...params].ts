641dbc8a1f53329156ea378c11fec331edb2e9fb
Sparsh <77487266+iSparshP@users.noreply.github.com>
2025-12-17 00:12:28 +0530

    fixed something

---
+++ src/pages/api/keystatic/[...params].ts
@@ -0,0 +1,8 @@
+import { makeHandler } from '@keystatic/astro/api';
+import config from '../../../../keystatic.config';
+
+export const all = makeHandler({
+    config,
+});
+
+export const prerender = false;
